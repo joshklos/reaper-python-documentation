8cf52bf1cdc87b3f03df804f9df8096b9948f011
joshklos <jklos@crossway.org>
2022-09-21 15:08:45 -0500

    Update check_for_armed.py

--- check_for_armed.py
+++ check_for_armed.py
@@ -1,12 +1,53 @@
 # Needs testing to verify that this is actually working as expected, at this time it appears to work.
 # Cycles through tracks in project and returns true if it finds any tracks with record armed turned on, otherwise returns false
 
+def track_states(trck):
+    status = {
+        'hide from mcp': False,
+        'hide from tcp': False,
+        'rec monitoring auto': False,
+        'rec monitoring on': False,
+        'rec armed': False,
+        'SIPd': False,
+        'soloed': False,
+        'muted': False,
+        'has fx enabled': False,
+        'selected': False,
+        'folder': False
+    }
+    bits = fn_bits(trck)
+    for bit in bits:
+        if bit == 1:
+            status['folder'] = True
+        elif bit == 2:
+            status['selected'] = True
+        elif bit == 4:
+            status['has fx enabled'] = True
+        elif bit == 8:
+            status['muted'] = True
+        elif bit == 16:
+            status['soloed'] = True
+        elif bit == 32:
+            status['SIPd'] = True
+        elif bit == 64:
+            status['rec armed'] = True
+        elif bit == 128:
+            status['rec monitoring on'] = True
+        elif bit == 256:
+            status['rec monitoring auto'] = True
+        elif bit == 512:
+            status['hide from tcp'] = True
+        elif bit == 1024:
+            status['hide from mcp'] = True
+    return status
+
 def check_for_armed():
     track_count = RPR_CountTracks(0)
     for x in range(0, track_count):
       tr = RPR_GetTrack(0, x)
       tr_name, track, track_state = RPR_GetTrackState(tr, 64)
-      if track_state == 192 or track_state == 194:
+      tatus = track_states(track_state)
+      if status['rec armed'] or status['rec monitoring on']:
         return True
     return False
     
